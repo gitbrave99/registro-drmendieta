047e99a53c098760c9621d362e1cd6f4568dfbde
gitbrave99 <bezisaac@gmail.com>
2023-02-20 21:43:32 -0600

    pagina asistencia, d/n in LocalStorage

--- assets/js/headerright-options.js
+++ assets/js/headerright-options.js
@@ -1,78 +1,105 @@
 function changeTheme() {
-    document.body.classList.toggle("dark-edition")
-    document.querySelectorAll(".selectPeriodoPrint").forEach(evt=>{
-        // evt.classList.toggle("selectbg")
-    })
+
+    document.body.classList.toggle("dark-edition");
+    document.querySelectorAll(".selectPeriodoPrint").forEach((evt) => { });
+
+    // d/n para modals
+    document.querySelectorAll(".modal-content").forEach((evt) => {
+        evt.classList.toggle("dark-edition");
+    });
+
+    allSelectsInPage = document.querySelectorAll(".selectItems");
+    if (allSelectsInPage != null) {
+        allSelectsInPage.forEach(item => {
+            item.classList.toggle("dark-edition")
+            item.classList.toggle("selectBGDark")
+            console.log("changin all");
+        })
+
+        // if (selectAddNotas.classList.contains("dark-edition")) {
+        //     selectAddNotas.setAttribute("id", "selectForPeriodos");
+        //     console.log("contiene");
+        // } else {
+        //     selectAddNotas.setAttribute("id", "");
+        //     console.log("NO contiene");
+        // }
+    }
 
 
-    document.querySelectorAll(".modal-content").forEach(evt=>{
-        evt.classList.toggle("dark-edition")
-        console.log("log modal");
-    })
+    document.getElementById("sidebardk").toggleAttribute("data-background-color");
 
-    document.getElementById("sidebardk").toggleAttribute("data-background-color")
+    sidebardk = document.getElementById("sidebardk");
 
-    sidebardk=document.getElementById("sidebardk")
-    
-    if(sidebardk.hasAttribute("data-background-color")){
-        if(sidebardk.getAttribute("data-background-color")=="black"){
-             console.log("changin atriu");
+    if (sidebardk.hasAttribute("data-background-color")) {
+        if (sidebardk.getAttribute("data-background-color") == "black") {
+            console.log("changin atriu");
         }
     }
-    // document.getElementById("sidebardk").toggleAttribute("black")
 }
- 
 
 function fullscrren() {
     // e.preventDefault();
-    document.body.classList.toggle("fullscreen-enable"), document.fullscreenElement || document
-        .mozFullScreenElement || document.webkitFullscreenElement ? document.cancelFullScreen ? document
-        .cancelFullScreen() : document.mozCancelFullScreen ? document.mozCancelFullScreen() : document
-        .webkitCancelFullScreen && document.webkitCancelFullScreen() : document.documentElement.requestFullscreen ?
-        document.documentElement.requestFullscreen() : document.documentElement.mozRequestFullScreen ? document
-        .documentElement.mozRequestFullScreen() : document.documentElement.webkitRequestFullscreen && document
-        .documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT)
+    document.body.classList.toggle("fullscreen-enable"),
+        document.fullscreenElement ||
+            document.mozFullScreenElement ||
+            document.webkitFullscreenElement
+            ? document.cancelFullScreen
+                ? document.cancelFullScreen()
+                : document.mozCancelFullScreen
+                    ? document.mozCancelFullScreen()
+                    : document.webkitCancelFullScreen && document.webkitCancelFullScreen()
+            : document.documentElement.requestFullscreen
+                ? document.documentElement.requestFullscreen()
+                : document.documentElement.mozRequestFullScreen
+                    ? document.documentElement.mozRequestFullScreen()
+                    : document.documentElement.webkitRequestFullscreen &&
+                    document.documentElement.webkitRequestFullscreen(
+                        Element.ALLOW_KEYBOARD_INPUT
+                    );
     console.log("pantalla completa");
 }
 
-
 // para cambiar tema los modas agregar el mismo toggle como al
 
+window.addEventListener("load", (event) => {
+    loadSavedTheme();
 
-window.addEventListener("load",event=>{
-    // saveThemeInLS()
-
-    document.querySelector("#completeScreen").addEventListener("click", evt => {
+    document.querySelector("#completeScreen").addEventListener("click", (evt) => {
         fullscrren();
-    })
-    
+    });
+
     document.querySelector("#changeTheme").addEventListener("click", (evt) => {
-        changeTheme()
-    })
+        changeTheme();
+        loadSavedTheme("change")
+    });
 });
 
-
 // guardar en LS el estado del theme
-activeTheme = ""
-boxContainerTheme = document.body
+activeTheme = "";
+boxContainerTheme = document.body;
 
-function saveThemeInLS() {
- 
-    if (!localStorage.getItem("activeTheme")) {
-        if (boxContainerTheme.classList.contains("dark-edition")) {
-            activeTheme = "dark-edition"
-        } else {
-            activeTheme = "light-edition"
+
+
+function loadSavedTheme(btnDarkLight="") {
+    if (localStorage.getItem("activeTheme")==null) {
+        saveThemeInLS("light-edition") 
+    } else { 
+        if(btnDarkLight==""){
+            activeTheme = localStorage.getItem("activeTheme");
+            if (activeTheme=="light-edition") {
+                boxContainerTheme.classList = "";
+            }else{
+                boxContainerTheme.classList.add(activeTheme);
+                document.getElementById("sidebardk").toggleAttribute("data-background-color");
+            }
+        }else{
+            activeTheme = boxContainerTheme.classList.contains("dark-edition")?"dark-edition":"light-edition"
+            saveThemeInLS(activeTheme)
         }
-        localStorage.setItem("activeTheme", activeTheme)
-        console.log("no existe");
-    } else {
-        console.log("si existe");
-        activeTheme = localStorage.getItem("activeTheme")
-        boxContainerTheme.classList=""
-        boxContainerTheme.classList.add(activeTheme)
+       
     }
-
-    
 }
 
+function saveThemeInLS(theme) {
+    localStorage.setItem("activeTheme", theme);
+}
